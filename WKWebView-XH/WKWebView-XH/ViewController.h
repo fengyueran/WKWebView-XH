3169dc0511b567ee3b725277d805af1445e7193a
xinghun meng <xinghunm@curacloudcorp.com>
2016-12-12 15:03:14 +0800

    init the repository

---
+++ WKWebView-XH/WKWebView-XH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebView-XH
+//
+//  Created by intern08 on 12/12/16.
+//  Copyright © 2016 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WKWebView-XH/WKWebView-XH/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WKWebView-XH
+//
+//  Created by intern08 on 12/12/16.
+//  Copyright © 2016 snow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
