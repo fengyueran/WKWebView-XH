ba3fb0990d825fece97fa76b0c77a2baf6e2394d
xinghun meng <xinghunm@curacloudcorp.com>
2016-12-12 15:06:56 +0800

    add WKWebView

--- WKWebView-XH/WKWebView-XH/ViewController.m
+++ WKWebView-XH/WKWebView-XH/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
 @interface ViewController ()
 
@@ -16,13 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    WKWebView *webView = [[WKWebView alloc]initWithFrame:self.view.bounds];
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
+    [self.view addSubview:webView];
 }
 
 
